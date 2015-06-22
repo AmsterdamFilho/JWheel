b982cbe7c3352b093cfa92fb8a223f96cbaddb41
Anmsterdam Luís de Lima Filho <amsterdam@luvva.com.br>
2015-06-22 16:42:36 -0300

    Created JMenuBarBuilder

---
+++ src/main/java/br/com/luvva/jwheel/swing/utils/JMenuBarBuilder.java
@@ -0,0 +1,95 @@
+package br.com.luvva.jwheel.swing.utils;
+
+import br.com.luvva.jwheel.swing.template.laf.JwLookAndFeel;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class JMenuBarBuilder
+{
+
+    private final JMenuBar     menuBar      = new JMenuBar();
+    private final Deque<JMenu> menus        = new ArrayDeque<>();
+    private final char         nullMnemonic = '\u0000';
+
+    @Inject
+    private JwLookAndFeel jwLookAndFeel;
+
+    @PostConstruct
+    public void init ()
+    {
+        menuBar.setBorder(jwLookAndFeel.getDefaultBorder());
+    }
+
+    public void addMenu (String menuText)
+    {
+        addMenu(menuText, nullMnemonic);
+    }
+
+    public void addMenu (String menuText, char mnemonic)
+    {
+        JMenu menu = new JMenu(menuText);
+        menus.addFirst(menu);
+        if (mnemonic != nullMnemonic)
+        {
+            menu.setMnemonic(mnemonic);
+        }
+        menuBar.add(menu);
+    }
+
+    public void addChildMenu (String childMenuText)
+    {
+        JMenu jMenu = new JMenu(childMenuText);
+        menus.peek().add(jMenu);
+        menus.addFirst(jMenu);
+    }
+
+    public void leaveChildMenu ()
+    {
+        menus.poll();
+    }
+
+    public void addMenuItem (String menuItemText, ActionListener actionListener)
+    {
+        addMenuItem(menuItemText, actionListener, "");
+    }
+
+    public void addMenuItem (String menuItemText, ActionListener actionListener, String keyCode)
+    {
+        addMenuItem(menuItemText, actionListener, keyCode, nullMnemonic);
+    }
+
+    public void addMenuItem (String menuItemText, ActionListener actionListener, String keyCode, char mnemonic)
+    {
+        JMenuItem menuItem = new JMenuItem(menuItemText);
+        if (!keyCode.isEmpty())
+        {
+            menuItem.setAccelerator(KeyStroke.getKeyStroke(keyCode));
+        }
+        if (mnemonic != nullMnemonic)
+        {
+            menuItem.setMnemonic(mnemonic);
+        }
+        menuItem.addActionListener(actionListener);
+        menus.peek().add(menuItem);
+    }
+
+    public void addHorizontalSeparator ()
+    {
+        menus.peek().add(jwLookAndFeel.createHorizontalJSeparator());
+    }
+
+    public void alignNextToTheRight ()
+    {
+        menuBar.add(Box.createHorizontalGlue());
+    }
+
+    public JMenuBar getMenuBar ()
+    {
+        return menuBar;
+    }
+}
