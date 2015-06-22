38b4b6e8806b5fbbf23b2b8cfdcbb5c807f8f6d1
Anmsterdam Luís de Lima Filho <amsterdam@luvva.com.br>
2015-06-22 16:44:11 -0300

    Created template border

---
+++ src/main/java/br/com/luvva/jwheel/swing/template/laf/BlackAndWhiteEtchedBorder.java
@@ -0,0 +1,14 @@
+package br.com.luvva.jwheel.swing.template.laf;
+
+import javax.swing.border.EtchedBorder;
+import java.awt.*;
+
+public class BlackAndWhiteEtchedBorder extends EtchedBorder
+{
+
+    public BlackAndWhiteEtchedBorder ()
+    {
+        super(Color.black, Color.white);
+    }
+
+}
