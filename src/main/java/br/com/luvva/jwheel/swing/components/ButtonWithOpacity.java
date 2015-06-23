5648d48cb1d9c7f898af1a424e432e7e11df8d9b
Anmsterdam Luís de Lima Filho <amsterdam@luvva.com.br>
2015-06-23 12:25:23 -0300

    Created ButtonWithOpacity class

---
+++ src/main/java/br/com/luvva/jwheel/swing/components/ButtonWithOpacity.java
@@ -0,0 +1,37 @@
+package br.com.luvva.jwheel.swing.components;
+
+import javax.swing.*;
+import java.awt.AlphaComposite;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+public class ButtonWithOpacity extends JButton
+{
+    private float opacity = 1f;
+
+    public ButtonWithOpacity ()
+    {
+        setHorizontalTextPosition(SwingConstants.CENTER);
+        setVerticalTextPosition(SwingConstants.BOTTOM);
+    }
+
+    public float getOpacity ()
+    {
+        return opacity;
+    }
+
+    public void setOpacity (float opacity)
+    {
+        this.opacity = opacity;
+        repaint();
+    }
+
+    @Override
+    public void paint (Graphics g)
+    {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, opacity));
+        super.paint(g2);
+        g2.dispose();
+    }
+}
