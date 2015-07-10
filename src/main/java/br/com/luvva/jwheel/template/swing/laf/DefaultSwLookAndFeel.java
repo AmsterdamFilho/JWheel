80303710c4b0ca5193cd002f2dd0207040f358e6
Anmsterdam Luís de Lima Filho <amsterdam@luvva.com.br>
2015-07-10 08:21:51 -0300

    Changed ApplicationScoped to Singleton

--- src/main/java/br/com/luvva/jwheel/template/swing/laf/DefaultSwLookAndFeel.java
+++ src/main/java/br/com/luvva/jwheel/template/swing/laf/DefaultSwLookAndFeel.java
@@ -1,12 +1,12 @@
 package br.com.luvva.jwheel.template.swing.laf;
 
-import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Singleton;
 import javax.swing.border.Border;
 
 /**
  * @author Lima Filho, A. L. - amsterdam@luvva.com.br
  */
-@ApplicationScoped
+@Singleton
 public class DefaultSwLookAndFeel implements SwLookAndFeel
 {
 
