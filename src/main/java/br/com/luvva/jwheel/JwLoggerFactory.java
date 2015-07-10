de218b22d3baeb5cfec79d2fe21d49792bdec78a
Anmsterdam Luís de Lima Filho <amsterdam@luvva.com.br>
2015-07-10 14:50:09 -0300

    Added flexibility to JWheelLoggerFactory

--- src/main/java/br/com/luvva/jwheel/JwLoggerFactory.java
+++ src/main/java/br/com/luvva/jwheel/JwLoggerFactory.java
@@ -1,11 +1,19 @@
 package br.com.luvva.jwheel;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.joran.spi.JoranException;
+import ch.qos.logback.core.util.StatusPrinter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
 import javax.inject.Singleton;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.nio.file.FileSystems;
 
 /**
  * @author Lima Filho, A. L. - amsterdam@luvva.com.br
@@ -14,10 +22,61 @@ import javax.inject.Singleton;
 public class JwLoggerFactory
 {
 
+    private Logger logger = LoggerFactory.getLogger(JwLoggerFactory.class);
+
     @Produces
     public Logger getLogger (InjectionPoint injectionPoint)
     {
         return LoggerFactory.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
+    }
+
+    public void configureLogback (String logFileName, String loggerLevel)
+    {
+        String userHome = System.getProperty("user.home");
+        //@formatter:off
+        configureLogback(
+                "<configuration>" +
+                    "<appender name=\"FILE\" class=\"ch.qos.logback.core.FileAppender\">" +
+                        "<file>" + userHome + FileSystems.getDefault().getSeparator() + logFileName + ".log</file>" +
+                        "<encoder>" +
+                            "<pattern>%date %level [%thread] %logger{10} [%file:%line] %msg%n</pattern>" +
+                        "</encoder>" +
+                    "</appender>" +
+                    "<root level=\"" + loggerLevel + "\">" +
+                        "<appender-ref ref=\"FILE\" />" +
+                    "</root>" +
+                "</configuration>"
+        );
+        //@formatter:on
+        logger.debug(userHome);
+    }
+
+    public void configureLogback (String xmlContent)
+    {
+        InputStream inputStream = null;
+        try
+        {
+            inputStream = new ByteArrayInputStream(xmlContent.getBytes("UTF-8"));
+        }
+        catch (UnsupportedEncodingException e)
+        {
+            logger.error("XmlContent can't be encoded to UTF-8", e);
+        }
+        LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+        try
+        {
+            JoranConfigurator configurator = new JoranConfigurator();
+            configurator.setContext(context);
+            // Call context.reset() to clear any previous configuration, e.g. default
+            // configuration. For multi-step configuration, omit calling context.reset().
+            context.reset();
+            configurator.doConfigure(inputStream);
+        }
+        catch (JoranException je)
+        {
+            // StatusPrinter will handle this
+        }
+        StatusPrinter.printInCaseOfErrorsOrWarnings(context);
     }
 
 }
