6535bdfb6595d81f82552e8cc62e7bb83e8dcf14
Anmsterdam Luís de Lima Filho <amsterdam@luvva.com.br>
2015-07-12 17:24:15 -0300

    JwLoggerFactory is now set in the AbstractTest class

---
+++ src/test/java/br/com/luvva/jwheel/AbstractTest.java
@@ -0,0 +1,20 @@
+package br.com.luvva.jwheel;
+
+import org.junit.runner.RunWith;
+
+import javax.annotation.PostConstruct;
+import java.nio.file.FileSystems;
+
+/**
+ * @author Amsterdam Filho - amsterdam@luvva.com.br
+ */
+@RunWith (WeldJUnit4Runner.class)
+public abstract class AbstractTest
+{
+    @PostConstruct
+    private void init ()
+    {
+        WeldContext.getInstance().getBean(JwLoggerFactory.class).configureLogbackAsDefault(JWheel.NAME
+                + FileSystems.getDefault().getSeparator() + JWheel.NAME);
+    }
+}
--- src/test/java/br/com/luvva/jwheel/template/swing/components/JwFrameTest.java
+++ src/test/java/br/com/luvva/jwheel/template/swing/components/JwFrameTest.java
@@ -1,21 +1,15 @@
 package br.com.luvva.jwheel.template.swing.components;
 
 import br.com.luvva.jwheel.AbstractTest;
-import br.com.luvva.jwheel.JwLoggerFactory;
 import br.com.luvva.jwheel.WeldContext;
 import org.junit.Test;
-
-import javax.inject.Inject;
 
 public class JwFrameTest extends AbstractTest
 {
 
-    private @Inject JwLoggerFactory jwLoggerFactory;
-
     @Test
     public void test () throws InterruptedException
     {
-        jwLoggerFactory.configureLogbackAsDefault("jwheel");
         JwFrame bean = WeldContext.getInstance().getBean(JwFrame.class);
         bean.setVisible(true);
 //        Thread.sleep(10000000);
