487dc8ba364abdbf9377f880f4e3b8225fce3dc1
Anmsterdam Luís de Lima Filho <amsterdam@luvva.com.br>
2015-07-17 14:01:19 -0300

    Added JavaLangUtils class

---
+++ src/main/java/br/com/luvva/jwheel/java/utils/JavaLangUtils.java
@@ -0,0 +1,24 @@
+package br.com.luvva.jwheel.java.utils;
+
+import java.lang.reflect.ParameterizedType;
+
+/**
+ * @author Lima Filho, A. L. - amsterdam@luvva.com.br
+ */
+public class JavaLangUtils
+{
+    private JavaLangUtils ()
+    {
+    }
+
+    public static <T> Class<T> getTypeArgumentClass (Class clazz)
+    {
+        return getTypeArgumentClass(clazz, 0);
+    }
+
+    public static <T> Class<T> getTypeArgumentClass (Class clazz, int index)
+    {
+       return (Class<T>) ((ParameterizedType) clazz.getGenericSuperclass()).getActualTypeArguments()[index];
+    }
+
+}
