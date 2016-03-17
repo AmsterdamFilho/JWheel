7b589e9dd1cc2ec61c73120416a388341c8814e6
Anmsterdam Luís de Lima Filho <amsterdam@luvva.com.br>
2016-03-17 19:37:01 -0300

    SystemUtils has now static methods, instead of being Singleton

--- src/main/java/br/com/luvva/jwheel/java/utils/SystemUtils.java
+++ src/main/java/br/com/luvva/jwheel/java/utils/SystemUtils.java
@@ -1,27 +1,25 @@
 package br.com.luvva.jwheel.java.utils;
 
-import javax.inject.Singleton;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 /**
  * @author Lima Filho, A. L. - amsterdam@luvva.com.br
  */
-@Singleton
 public class SystemUtils
 {
 
-    public boolean isOsX ()
+    public static boolean isOsX ()
     {
         return System.getProperty("os.name").toLowerCase().contains("mac");
     }
 
-    public boolean isWindows ()
+    public static boolean isWindows ()
     {
         return System.getProperty("os.name").toLowerCase().contains("windows");
     }
 
-    public String getLocalhostName () throws UnknownHostException
+    public static String getLocalhostName () throws UnknownHostException
     {
         return InetAddress.getLocalHost().getHostName();
     }
