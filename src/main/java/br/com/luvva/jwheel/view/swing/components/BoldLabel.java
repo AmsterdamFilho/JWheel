c94151884efe75b47fd0df7f2529cb59acd7228c
Anmsterdam Luís de Lima Filho <amsterdam@luvva.com.br>
2016-03-17 15:06:41 -0300

    Created BoldLabel

---
+++ src/main/java/br/com/luvva/jwheel/view/swing/components/BoldLabel.java
@@ -0,0 +1,27 @@
+package br.com.luvva.jwheel.view.swing.components;
+
+import javax.swing.*;
+import java.awt.Font;
+
+/**
+ * @author Lima Filho, A. L. - amsterdam@luvva.com.br
+ */
+public class BoldLabel extends JLabel
+{
+    public BoldLabel ()
+    {
+        this("");
+    }
+
+    public BoldLabel (String text)
+    {
+        this(text, SwingConstants.LEFT);
+    }
+
+    public BoldLabel (String text, int alignment)
+    {
+        super(text);
+        setFont(getFont().deriveFont(Font.BOLD));
+        setHorizontalAlignment(alignment);
+    }
+}
