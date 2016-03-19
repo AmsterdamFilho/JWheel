d3afba8f01b1d29b55dd8ad9511e854f004fbb1c
Anmsterdam Luís de Lima Filho <amsterdam@luvva.com.br>
2016-03-19 18:32:23 -0300

    Added TextChangedListener utility class

---
+++ src/main/java/br/com/luvva/jwheel/view/swing/utils/TextChangedListener.java
@@ -0,0 +1,31 @@
+package br.com.luvva.jwheel.view.swing.utils;
+
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
+/**
+ * @author Lima Filho, A. L. - amsterdam@luvva.com.br
+ */
+public abstract class TextChangedListener implements DocumentListener
+{
+
+    @Override
+    public void insertUpdate (DocumentEvent e)
+    {
+        textChanged(e);
+    }
+
+    @Override
+    public void removeUpdate (DocumentEvent e)
+    {
+        textChanged(e);
+    }
+
+    @Override
+    public void changedUpdate (DocumentEvent e)
+    {
+        textChanged(e);
+    }
+
+    public abstract void textChanged (DocumentEvent de);
+}
