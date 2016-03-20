ec1acbd3b6e31908e3428ddd5f7106109d0e2182
Anmsterdam Luís de Lima Filho <amsterdam@luvva.com.br>
2016-03-20 17:57:24 -0300

    Added BooleanWrapper

---
+++ src/main/java/br/com/luvva/jwheel/model/utils/BooleanWrapper.java
@@ -0,0 +1,19 @@
+package br.com.luvva.jwheel.model.utils;
+
+/**
+ * @author Lima Filho, A. L. - amsterdam@luvva.com.br
+ */
+public class BooleanWrapper
+{
+    private boolean value;
+
+    public boolean isValue ()
+    {
+        return value;
+    }
+
+    public void setValue (boolean value)
+    {
+        this.value = value;
+    }
+}
