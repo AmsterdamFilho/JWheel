3b30c2cca70a016951905b28a083f8ff460a30b9
Anmsterdam Luís de Lima Filho <amsterdam@luvva.com.br>
2016-03-23 15:25:59 -0300

    JwScrollPane created

---
+++ src/main/java/br/com/luvva/jwheel/view/swing/components/JwScrollPane.java
@@ -0,0 +1,302 @@
+package br.com.luvva.jwheel.view.swing.components;
+
+import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.Component;
+
+/**
+ * A JScrollPane with a method that adjusts the scroll bars value so that the selected portion of the Viewport View can
+ * become visible.
+ *
+ * @author Lima Filho, A. L. - amsterdam@luvva.com.br
+ */
+public class JwScrollPane extends JScrollPane
+{
+
+    private FriendlyView                    friendlyView;
+    private JwScrollPaneFriendlyViewAdapter horizontalAdapter;
+    private JwScrollPaneFriendlyViewAdapter verticalAdapter;
+
+    public JwScrollPane ()
+    {
+        setDefaultPolicies();
+    }
+
+    public JwScrollPane (Component view)
+    {
+        super(view);
+        setDefaultPolicies();
+    }
+
+    private void setDefaultPolicies ()
+    {
+        setHorizontalScrollBarPolicy(HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        setVerticalScrollBarPolicy(VERTICAL_SCROLLBAR_ALWAYS);
+    }
+
+    @Override
+    public void setViewportView (Component view)
+    {
+        super.setViewportView(view);
+        if (view instanceof FriendlyView)
+        {
+            friendlyView = (FriendlyView) view;
+            createHorizontalAdapter();
+            createVerticalAdapter();
+        }
+    }
+
+    public void setAsFriendlyViewportView (JList jList)
+    {
+        setViewportView(new FriendlyViewWrapper(jList)
+        {
+            @Override
+            public boolean shouldUpdateVertically ()
+            {
+                return true;
+            }
+
+            @Override
+            public boolean shouldUpdateHorizontally ()
+            {
+                return false;
+            }
+
+            @Override
+            public int getVisibleHeight ()
+            {
+                return jList.getVisibleRect().height;
+            }
+
+            @Override
+            public int getVerticalUnitsCount ()
+            {
+                return jList.getModel().getSize();
+            }
+
+            @Override
+            public int getSelectedVerticalUnitIndex ()
+            {
+                return 0;
+            }
+
+            @Override
+            public int getVisibleWidth ()
+            {
+                return 0;
+            }
+
+            @Override
+            public int getHorizontalUnitsCount ()
+            {
+                return 0;
+            }
+
+            @Override
+            public int getSelectedHorizontalUnitIndex ()
+            {
+                return -1;
+            }
+        });
+        jList.addListSelectionListener(e -> updateScrollBars());
+    }
+
+    public void setAsFriendlyViewportView (JTable jTable)
+    {
+        setViewportView(new FriendlyViewWrapper(jTable)
+        {
+            @Override
+            public boolean shouldUpdateVertically ()
+            {
+                return true;
+            }
+
+            @Override
+            public boolean shouldUpdateHorizontally ()
+            {
+                return false;
+            }
+
+            @Override
+            public int getVisibleHeight ()
+            {
+                return jTable.getVisibleRect().height;
+            }
+
+            @Override
+            public int getVerticalUnitsCount ()
+            {
+                return jTable.getModel().getRowCount();
+            }
+
+            @Override
+            public int getSelectedVerticalUnitIndex ()
+            {
+                return 0;
+            }
+
+            @Override
+            public int getVisibleWidth ()
+            {
+                return 0;
+            }
+
+            @Override
+            public int getHorizontalUnitsCount ()
+            {
+                return 0;
+            }
+
+            @Override
+            public int getSelectedHorizontalUnitIndex ()
+            {
+                return 0;
+            }
+        });
+        jTable.getSelectionModel().addListSelectionListener(e -> updateScrollBars());
+    }
+
+    public void updateScrollBars ()
+    {
+        if (friendlyView.shouldUpdateHorizontally())
+        {
+            updateScrollBar(getHorizontalScrollBar(), horizontalAdapter);
+        }
+        if (friendlyView.shouldUpdateVertically())
+        {
+            updateScrollBar(getVerticalScrollBar(), verticalAdapter);
+        }
+    }
+
+    private void updateScrollBar (JScrollBar scrollBar, JwScrollPaneFriendlyViewAdapter adapter)
+    {
+        int selectedIndex = adapter.getSelectedUnitIndex();
+        if (selectedIndex < 0)
+        {
+            return;
+        }
+        int scrollBarValue = scrollBar.getValue();
+        int visibleSize = adapter.getVisibleSize();
+        int viewUnitSize = adapter.getSize() / adapter.getUnitsCount();
+        int selectedUnitCoordinate = selectedIndex * viewUnitSize;
+
+        if (selectedUnitCoordinate + viewUnitSize >= scrollBarValue + visibleSize)
+        {
+            scrollBar.setValue(selectedUnitCoordinate - visibleSize + viewUnitSize);
+        }
+        else if (selectedUnitCoordinate < scrollBarValue)
+        {
+            scrollBar.setValue(selectedUnitCoordinate);
+        }
+        else
+        {
+            // selected portion of the view is already visible
+        }
+    }
+
+    private void createHorizontalAdapter ()
+    {
+        horizontalAdapter = new JwScrollPaneFriendlyViewAdapter()
+        {
+            @Override
+            public int getVisibleSize ()
+            {
+                return friendlyView.getVisibleWidth();
+            }
+
+            @Override
+            public int getSize ()
+            {
+                return friendlyView.getWidth();
+            }
+
+            @Override
+            public int getUnitsCount ()
+            {
+                return friendlyView.getHorizontalUnitsCount();
+            }
+
+            @Override
+            public int getSelectedUnitIndex ()
+            {
+                return friendlyView.getSelectedHorizontalUnitIndex();
+            }
+        };
+    }
+
+    private void createVerticalAdapter ()
+    {
+        verticalAdapter = new JwScrollPaneFriendlyViewAdapter()
+        {
+            @Override
+            public int getVisibleSize ()
+            {
+                return friendlyView.getVisibleHeight();
+            }
+
+            @Override
+            public int getSize ()
+            {
+                return friendlyView.getHeight();
+            }
+
+            @Override
+            public int getUnitsCount ()
+            {
+                return friendlyView.getVerticalUnitsCount();
+            }
+
+            @Override
+            public int getSelectedUnitIndex ()
+            {
+                return friendlyView.getSelectedVerticalUnitIndex();
+            }
+        };
+    }
+
+    private interface JwScrollPaneFriendlyViewAdapter
+    {
+        int getVisibleSize ();
+
+        int getSize ();
+
+        int getUnitsCount ();
+
+        int getSelectedUnitIndex ();
+    }
+
+    public static abstract class FriendlyView extends JPanel
+    {
+        public abstract boolean shouldUpdateVertically ();
+
+        public abstract boolean shouldUpdateHorizontally ();
+
+        public int getVisibleHeight ()
+        {
+            return getVisibleRect().height;
+        }
+
+        public abstract int getVerticalUnitsCount ();
+
+        public abstract int getSelectedVerticalUnitIndex ();
+
+        public int getVisibleWidth ()
+        {
+            return getVisibleRect().width;
+        }
+
+        public abstract int getHorizontalUnitsCount ();
+
+        public abstract int getSelectedHorizontalUnitIndex ();
+    }
+
+    public static abstract class FriendlyViewWrapper extends FriendlyView
+    {
+        public FriendlyViewWrapper (Component component)
+        {
+            setLayout(new BorderLayout());
+            add(component, BorderLayout.CENTER);
+        }
+    }
+
+}
---
+++ src/test/java/br/com/luvva/jwheel/view/swing/components/JwScrollPaneTest.java
@@ -0,0 +1,177 @@
+package br.com.luvva.jwheel.view.swing.components;
+
+import org.junit.Test;
+
+import javax.swing.*;
+import javax.swing.text.JTextComponent;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.GridLayout;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Lima Filho, A. L. - amsterdam@luvva.com.br
+ */
+public class JwScrollPaneTest
+{
+
+    @Test
+    public void testUpdateScrollBars () throws Exception
+    {
+        EventQueue.invokeLater(this::showTestFrame);
+//        Thread.sleep(1000000000);
+    }
+
+    private void showTestFrame ()
+    {
+        JwScrollPane jwScrollPane = new JwScrollPane();
+        jwScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+        jwScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        FriendlyView textFieldsPanel = new FriendlyView(jwScrollPane);
+        jwScrollPane.setViewportView(textFieldsPanel);
+
+        BoldLabel infoLabel = new BoldLabel("Type an index from 0 to 99 and press enter to select a Text Field");
+        JTextField indexTextField = new JTextField();
+        indexTextField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased (KeyEvent e)
+            {
+                if (e.getKeyChar() == '\n')
+                {
+                    String text = indexTextField.getText();
+                    if (text.matches("[0-9]{1,2}"))
+                    {
+                        textFieldsPanel.requestFocus(Integer.valueOf(text));
+                    }
+                    else
+                    {
+                        JOptionPane.showMessageDialog(null, "Invalid index!");
+                    }
+                }
+            }
+        });
+        indexTextField.setColumns(2);
+        JPanel pageEndPanel = new JPanel();
+        pageEndPanel.add(infoLabel);
+        pageEndPanel.add(indexTextField);
+
+        JPanel contentPane = new JPanel(new BorderLayout());
+        contentPane.add(jwScrollPane, BorderLayout.CENTER);
+        contentPane.add(pageEndPanel, BorderLayout.PAGE_START);
+
+        JFrame jFrame = new JFrame();
+        jFrame.setTitle("JwScrollPaneTest");
+        jFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        jFrame.setContentPane(contentPane);
+        jFrame.setPreferredSize(new Dimension(600, 350));
+        jFrame.setMinimumSize(new Dimension(600, 350));
+        jFrame.setMaximumSize(new Dimension(600, 350));
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+
+    private class TextFieldForTheTest extends JwTextField
+    {
+        private JwScrollPane jwScrollPane;
+        private FriendlyView friendlyView;
+        private int index;
+
+        private TextFieldForTheTest (JwScrollPane jwScrollPane, FriendlyView friendlyView, int index)
+        {
+            this.jwScrollPane = jwScrollPane;
+            addFocusListener(new FocusAdapter() {
+                @Override
+                public void focusGained (FocusEvent e)
+                {
+                    friendlyView.setSelectedIndex(index);
+                    jwScrollPane.updateScrollBars();
+                }
+
+                @Override
+                public void focusLost (FocusEvent e)
+                {
+                    friendlyView.setSelectedIndex(-1);
+                }
+            });
+            this.index = index;
+            setMinimumSize(new Dimension(100, 60));
+            setMaximumSize(new Dimension(100, 60));
+            setPreferredSize(new Dimension(100, 60));
+        }
+    }
+
+    private class FriendlyView extends JwScrollPane.FriendlyView
+    {
+
+        private List<JTextComponent> jTextComponents = new ArrayList<>();
+        private int selectedIndex = -1;
+
+        private FriendlyView (JwScrollPane jwScrollPane)
+        {
+            setLayout(new GridLayout(10, 10));
+            for (int i = 0; i < 100; i++)
+            {
+                TextFieldForTheTest txt = new TextFieldForTheTest(jwScrollPane, this, i);
+                add(txt);
+                jTextComponents.add(txt);
+            }
+        }
+
+        private void requestFocus (int textFieldIndex)
+        {
+            jTextComponents.get(textFieldIndex).requestFocusInWindow();
+        }
+
+        private void setSelectedIndex (int selectedIndex)
+        {
+            this.selectedIndex = selectedIndex;
+        }
+
+        @Override
+        public boolean shouldUpdateVertically ()
+        {
+            return true;
+        }
+
+        @Override
+        public boolean shouldUpdateHorizontally ()
+        {
+            return true;
+        }
+
+        @Override
+        public int getVerticalUnitsCount ()
+        {
+            return 10;
+        }
+
+        @Override
+        public int getSelectedVerticalUnitIndex ()
+        {
+            if (selectedIndex == -1)
+            {
+                return -1;
+            }
+            return selectedIndex / 10;
+        }
+
+        @Override
+        public int getHorizontalUnitsCount ()
+        {
+            return 10;
+        }
+
+        @Override
+        public int getSelectedHorizontalUnitIndex ()
+        {
+            return selectedIndex % 10;
+        }
+    }
+
+}
