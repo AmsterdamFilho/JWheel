9f26f1f48e50a91007024efcd1a949189f4a01dc
Anmsterdam Luís de Lima Filho <amsterdam@luvva.com.br>
2016-03-25 15:21:35 -0300

    AbstractTest now uses default logger configuration

--- src/test/java/br/com/luvva/jwheel/AbstractTest.java
+++ src/test/java/br/com/luvva/jwheel/AbstractTest.java
@@ -1,12 +1,10 @@
 package br.com.luvva.jwheel;
 
 import br.com.luvva.jwheel.model.beans.LogParameters;
-import ch.qos.logback.classic.Level;
 import org.junit.runner.RunWith;
 
 import javax.annotation.PostConstruct;
-import javax.enterprise.inject.Default;
-import javax.enterprise.util.AnnotationLiteral;
+import javax.inject.Inject;
 
 /**
  * @author Amsterdam Filho - amsterdam@luvva.com.br
@@ -14,11 +12,13 @@ import javax.enterprise.util.AnnotationLiteral;
 @RunWith (WeldJUnit4Runner.class)
 public abstract class AbstractTest
 {
+
+    private @Inject JwLoggerFactory jwLoggerFactory;
+    private @Inject LogParameters   logParameters;
+
     @PostConstruct
     private void init ()
     {
-        LogParameters lp = WeldContext.getInstance().getBean(LogParameters.class, new AnnotationLiteral<Default>() {});
-        WeldContext.getInstance().getBean(JwLoggerFactory.class)
-                   .configureLogbackAsDefault(lp.getLogFilePath(), Level.INFO);
+        jwLoggerFactory.configure(logParameters);
     }
 }
