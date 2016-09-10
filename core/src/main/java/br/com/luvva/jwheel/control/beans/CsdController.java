8c438ef26ab7704e9a841c93fb51fbe7e478a43f
Anmsterdam Luís de Lima Filho <amsterdam@luvva.com.br>
2016-09-09 21:43:52 -0300

    Extracted CsdService class from CsdController

--- core/src/main/java/br/com/luvva/jwheel/control/beans/CsdController.java
+++ core/src/main/java/br/com/luvva/jwheel/control/beans/CsdController.java
@@ -1,12 +1,9 @@
 package br.com.luvva.jwheel.control.beans;
 
-import br.com.luvva.jwheel.WeldContext;
 import br.com.luvva.jwheel.control.DefaultController;
-import br.com.luvva.jwheel.dao.jpa.ConnectionTester;
-import br.com.luvva.jwheel.dao.jpa.EntityManagerProducer;
-import br.com.luvva.jwheel.dao.xstream.ConnectionParametersDao;
 import br.com.luvva.jwheel.model.beans.ConnectionParameters;
 import br.com.luvva.jwheel.model.i18n.TextProvider;
+import br.com.luvva.jwheel.services.beans.CsdService;
 import br.com.luvva.jwheel.view.javafx.utils.AlertProducer;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
@@ -14,7 +11,6 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import org.slf4j.Logger;
 
 import javax.inject.Inject;
 
@@ -28,17 +24,14 @@ public class CsdController extends DefaultController
     private @FXML TextField        txtUser;
     private @FXML PasswordField    txtPassword;
 
-    private @Inject ConnectionParameters    connectionParameters;
-    private @Inject EntityManagerProducer   entityManagerProducer;
-    private @Inject ConnectionParametersDao dao;
-    private @Inject Logger                  logger;
-    private @Inject AlertProducer           alertProducer;
-    private @Inject TextProvider            textProvider;
+    private @Inject ConnectionParameters connectionParameters;
+    private @Inject CsdService           csdService;
+    private @Inject AlertProducer        alertProducer;
+    private @Inject TextProvider         textProvider;
 
     public void testConnection ()
     {
-        entityManagerProducer.init(connectionParameters);
-        if (WeldContext.getInstance().getBean(ConnectionTester.class).execute())
+        if (csdService.testConnection(connectionParameters))
         {
             alertProducer.showSuccessAlert(textProvider.getText(TextProvider.cs_testSucceeded));
         }
@@ -46,23 +39,14 @@ public class CsdController extends DefaultController
         {
             alertProducer.showErrorAlert(textProvider.getText(TextProvider.cs_testFailed));
         }
-        entityManagerProducer.init();
     }
 
     public void saveAndExit ()
     {
-        try
+        if (csdService.saveOk(connectionParameters))
         {
-            dao.merge(connectionParameters);
-            alertProducer.showSuccessAlert(textProvider.getText(TextProvider.g_saveSucceeded));
             exit();
         }
-        catch (Exception e)
-        {
-            alertProducer.showErrorAlert(textProvider.getText(TextProvider.g_saveFailed));
-            logger.error("Could not persist Connection Parameters", e);
-        }
-        entityManagerProducer.init();
     }
 
     public void exit ()
