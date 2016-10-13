4cc6de1bf1d3a1983040325943a8dbb163f89443
Amsterdam Luís de Lima Filho <amsterdam@luvva.com.br>
2016-10-13 10:18:03 -0300

    Removed test that was not automated

--- template/src/test/java/br/com/luvva/jwheel/control/ApplicationStarterTest.java
+++ template/src/test/java/br/com/luvva/jwheel/control/ApplicationStarterTest.java
@@ -1,16 +1,14 @@
 package br.com.luvva.jwheel.control;
 
 import br.com.luvva.jwheel.template.control.ApplicationStarter;
-import org.junit.Test;
 
 /**
  * @author Lima Filho, A. L. - amsterdam@luvva.com.br
  */
 public class ApplicationStarterTest
 {
-    @Test
-    public void test ()
+    public static void main (String[] args)
     {
-        ApplicationStarter.main(null);
+        ApplicationStarter.main(args);
     }
 }
