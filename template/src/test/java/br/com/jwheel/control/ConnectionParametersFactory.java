4e169ead6f421c8e9a81e53cb413cedeb6c52301
Amsterdam Luís de Lima Filho <amsterdam@luvva.com.br>
2016-11-22 23:30:08 -0200

    Removed default ConnectionParametersFactory

---
+++ template/src/test/java/br/com/jwheel/control/ConnectionParametersFactory.java
@@ -0,0 +1,31 @@
+package br.com.jwheel.control;
+
+import br.com.jwheel.jpa.ConnectionParameters;
+import br.com.jwheel.jpa.ConnectionParametersDao;
+import br.com.jwheel.xml.service.PreferencesFactoryFromXml;
+
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+
+/**
+ * @author Lima Filho, A. L. - amsterdam@luvva.com.br
+ */
+public class ConnectionParametersFactory extends PreferencesFactoryFromXml<ConnectionParameters>
+{
+    private @Inject ConnectionParametersDao dao;
+
+    @Produces
+    public ConnectionParameters produce ()
+    {
+        return produce(dao);
+    }
+
+    @Override
+    public void setDefaultPreferences (ConnectionParameters parameters)
+    {
+        parameters.setDriver("org.postgresql.Driver");
+        parameters.setPassword("postgres");
+        parameters.setUrl("jdbc:postgresql://localhost:5433/postgres");
+        parameters.setUser("postgres");
+    }
+}
