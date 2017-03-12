83e264390cd5fb5627e4ebc5cdb1f1d3de120689
Amsterdam Luís de Lima Filho <amsterdam@luvva.com.br>
2017-03-12 10:23:23 -0300

    Removed auto complete classes committed by mistake

--- core/src/main/java/br/com/jwheel/core/service/autocomplete/AcListService.java
+++
@@ -1,8 +0,0 @@
-package br.com.jwheel.core.service.autocomplete;
-
-/**
- * @author Lima Filho, A. L. - amsterdam@luvva.com.br
- */
-public abstract class AcListService<T> extends AutoCompleteService<T, String>
-{
-}
--- core/src/main/java/br/com/jwheel/core/service/autocomplete/AcTableService.java
+++
@@ -1,9 +0,0 @@
-package br.com.jwheel.core.service.autocomplete;
-
-/**
- * @author Lima Filho, A. L. - amsterdam@luvva.com.br
- */
-public abstract class AcTableService<T> extends AutoCompleteService<T, Object[]>
-{
-    public abstract String[] getHeaderTitles ();
-}
